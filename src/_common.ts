0347fd28fad8e649b4b32eabc9b81e5e762a2f9c
Tim Smart <hello@timsmart.co>
2023-04-25 21:40:27 +1200

    chore: add bot

---
+++ src/_common.ts
@@ -0,0 +1,11 @@
+export * as Config from "@effect/io/Config"
+export * as Data from "@effect/data/Data"
+export * as Effect from "@effect/io/Effect"
+export * as HashMap from "@effect/data/HashMap"
+export * as Layer from "@effect/io/Layer"
+export { Tag } from "@effect/data/Context"
+export * as Option from "@effect/data/Option"
+
+export { flow, pipe, identity } from "@effect/data/Function"
+
+export { Discord } from "dfx"
---
+++ src/main.ts
@@ -0,0 +1,193 @@
+import {
+  Config,
+  Data,
+  Effect,
+  HashMap,
+  Layer,
+  Option,
+  Tag,
+  pipe,
+} from "bot/_common"
+import { Discord, DiscordREST, Intents, Ix, IxHelpers, Perms, UI } from "dfx"
+import * as Cache from "dfx/Cache"
+import * as CacheP from "dfx/Cache/prelude"
+import { DiscordGateway } from "dfx/DiscordGateway"
+import { makeLive, runIx } from "dfx/gateway"
+import { PermissionFlag } from "dfx/types"
+import * as Dotenv from "dotenv"
+
+Dotenv.config()
+
+// ==== channels cache
+const makeChannelsCache = CacheP.channels(Cache.memoryParentDriver())
+interface ChannelsCache
+  extends Effect.Effect.Success<typeof makeChannelsCache> {}
+const ChannelsCache = Tag<ChannelsCache>()
+const ChannelsCacheLive = Layer.effect(ChannelsCache, makeChannelsCache)
+
+// ==== deps
+const BotLive = makeLive({
+  token: Config.secret("DISCORD_BOT_TOKEN"),
+  gateway: {
+    intents: Config.succeed(Intents.fromList(["GUILD_MESSAGES", "GUILDS"])),
+  },
+})
+
+const EnvLive = Layer.provideMerge(BotLive, ChannelsCacheLive)
+
+// ==== errors
+class NotValidMessageError extends Data.TaggedClass("NotValidMessageError")<{
+  readonly reason: "non-default" | "non-text-channel" | "disabled"
+}> {}
+
+class PermissionsError extends Data.TaggedClass("PermissionsError")<{
+  readonly reason: "edit-message"
+}> {}
+
+const program = Effect.gen(function* ($) {
+  const gateway = yield* $(DiscordGateway)
+  const rest = yield* $(DiscordREST)
+  const channels = yield* $(ChannelsCache)
+
+  const handleMessages = gateway.handleDispatch("MESSAGE_CREATE", message =>
+    pipe(
+      Effect.allPar({
+        message: Effect.cond(
+          () => message.type === Discord.MessageType.DEFAULT,
+          () => message,
+          () => new NotValidMessageError({ reason: "non-default" }),
+        ),
+        channel: channels.get(message.guild_id!, message.channel_id),
+      }),
+      Effect.filterOrFail(
+        ({ channel }) => channel.type === Discord.ChannelType.GUILD_TEXT,
+        () => new NotValidMessageError({ reason: "non-text-channel" }),
+      ),
+      Effect.filterOrFail(
+        ({ channel }) => channel.topic?.includes("[threads]") === true,
+        () => new NotValidMessageError({ reason: "disabled" }),
+      ),
+      Effect.flatMap(({ channel }) =>
+        rest.startThreadFromMessage(channel.id, message.id, {
+          name: `${message.member!.nick}'s thread`,
+        }),
+      ),
+      Effect.flatMap(_ => _.json),
+      Effect.flatMap(thread =>
+        rest.createMessage(thread.id, {
+          components: UI.grid([
+            [
+              UI.button({
+                custom_id: `edit_${message.author.id}`,
+                label: "Edit title",
+              }),
+              UI.button({
+                custom_id: `archive_${message.author.id}`,
+                label: "Archive",
+                style: Discord.ButtonStyle.SECONDARY,
+              }),
+            ],
+          ]),
+        }),
+      ),
+      Effect.catchAllCause(Effect.logErrorCause),
+    ),
+  )
+
+  const hasManage = Perms.has(PermissionFlag.MANAGE_CHANNELS)
+
+  const checkPermissions = Effect.gen(function* ($) {
+    const ix = yield* $(Ix.interaction)
+    const ctx = yield* $(Ix.MessageComponentContext)
+    const authorId = ctx.custom_id.split("_")[1]
+    const canEdit =
+      authorId === ix.member?.user?.id || hasManage(ix.member!.permissions!)
+
+    if (!canEdit) {
+      yield* $(Effect.fail(new PermissionsError({ reason: "edit-message" })))
+    }
+
+    return [ix, ctx] as const
+  })
+
+  const handlePermissionsError = () =>
+    Effect.succeed(
+      Ix.r({
+        type: Discord.InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          flags: Discord.MessageFlag.EPHEMERAL,
+          content: "You don't have permissions to edit this thread",
+        },
+      }),
+    )
+
+  const edit = Ix.messageComponent(
+    Ix.idStartsWith("edit_"),
+    pipe(
+      checkPermissions,
+      Effect.as(
+        Ix.r({
+          type: Discord.InteractionCallbackType.MODAL,
+          data: {
+            custom_id: "edit",
+            title: "Edit title",
+            components: UI.singleColumn([
+              UI.textInput({ custom_id: "title", label: "New title" }),
+            ]),
+          },
+        }),
+      ),
+      Effect.catchTag("PermissionsError", handlePermissionsError),
+    ),
+  )
+
+  const editModal = Ix.modalSubmit(
+    Ix.id("edit"),
+    pipe(
+      Effect.allPar({
+        title: Effect.flatMap(Ix.modalValues, _ =>
+          Option.flatMap(HashMap.get(_, "title"), Option.fromNullable),
+        ),
+        context: Ix.interaction,
+      }),
+      Effect.flatMap(({ title, context }) =>
+        rest.modifyChannel(context.channel_id!, { name: title }),
+      ),
+      Effect.as(
+        IxHelpers.response({
+          type: Discord.InteractionCallbackType.DEFERRED_UPDATE_MESSAGE,
+        }),
+      ),
+    ),
+  )
+
+  const archive = Ix.messageComponent(
+    Ix.idStartsWith("archive_"),
+    pipe(
+      checkPermissions,
+      Effect.tap(([ix]) =>
+        rest.modifyChannel(ix.channel_id!, {
+          archived: true,
+        }),
+      ),
+      Effect.as({
+        type: Discord.InteractionCallbackType.DEFERRED_UPDATE_MESSAGE,
+      }),
+      Effect.catchTag("PermissionsError", handlePermissionsError),
+    ),
+  )
+
+  const run = pipe(
+    Ix.builder.add(archive).add(edit).add(editModal),
+    runIx(Effect.catchAllCause(Effect.logErrorCause)),
+  )
+
+  yield* $(Effect.allParDiscard(handleMessages, channels.run, run))
+})
+
+pipe(
+  program,
+  Effect.provideLayer(EnvLive),
+  Effect.tapErrorCause(Effect.logErrorCause),
+  Effect.runFork,
+)
