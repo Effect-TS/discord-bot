ce045fedca3286fc18c5c3b0c48f12b7f1a20fc5
Tim Smart <hello@timsmart.co>
2023-05-05 11:10:27 +1200

    refactor: PermissionsError

--- src/AutoThreads.ts
+++ src/AutoThreads.ts
@@ -36,6 +36,11 @@ export class NotValidMessageError extends Data.TaggedClass(
   readonly reason: "non-default" | "from-bot" | "non-text-channel" | "disabled"
 }> {}
 
+export class PermissionsError extends Data.TaggedClass("PermissionsError")<{
+  readonly action: string
+  readonly subject: string
+}> {}
+
 export interface AutoThreadsOptions {
   readonly topicKeyword: string
 }
@@ -137,13 +142,11 @@ const make = ({ topicKeyword }: AutoThreadsOptions) =>
           authorId === ix.member?.user?.id || hasManage(ix.member!.permissions!)
 
         if (!canEdit) {
-          return Ix.response({
-            type: Discord.InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE,
-            data: {
-              flags: Discord.MessageFlag.EPHEMERAL,
-              content: "You don't have permissions to edit this thread",
-            },
-          })
+          yield* _(
+            Effect.fail(
+              new PermissionsError({ action: "edit", subject: "thread" }),
+            ),
+          )
         }
 
         return yield* $(self)
@@ -175,7 +178,7 @@ const make = ({ topicKeyword }: AutoThreadsOptions) =>
       ),
     )
 
-    const editModal = Ix.modalSubmit(
+    const editSubmit = Ix.modalSubmit(
       Ix.id("edit"),
       pipe(
         Effect.allPar({
@@ -201,7 +204,6 @@ const make = ({ topicKeyword }: AutoThreadsOptions) =>
       Ix.idStartsWith("archive_"),
       pipe(
         Ix.Interaction,
-        Effect.zipLeft(Effect.die("boom")),
         Effect.tap(ix =>
           rest.modifyChannel(ix.channel_id!, { archived: true }),
         ),
@@ -214,16 +216,24 @@ const make = ({ topicKeyword }: AutoThreadsOptions) =>
       ),
     )
 
-    yield* _(
-      registry.register(
-        Ix.builder
-          .add(archive)
-          .add(edit)
-          .add(editModal)
-          .catchAllCause(Effect.logErrorCause),
-      ),
-    )
+    const ix = Ix.builder
+      .add(archive)
+      .add(edit)
+      .add(editSubmit)
+      .catchTagRespond("PermissionsError", _ =>
+        Effect.succeed(
+          Ix.response({
+            type: Discord.InteractionCallbackType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+              flags: Discord.MessageFlag.EPHEMERAL,
+              content: `You don't have permission to ${_.action} this ${_.subject}.`,
+            },
+          }),
+        ),
+      )
+      .catchAllCause(Effect.logErrorCause)
 
+    yield* _(registry.register(ix))
     yield* _(handleMessages)
   })
 
