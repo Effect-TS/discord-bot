92514d0619a77b6621c6d82b543a338e73ee41fb
Tim <hello@timsmart.co>
2023-05-22 16:09:05 +1200

    chore: Summarizer cleanup (#16)

--- src/MemberCache.ts
+++ src/MemberCache.ts
@@ -1,16 +1,5 @@
-import {
-  Cache,
-  Data,
-  Duration,
-  Effect,
-  Layer,
-  Request,
-  RequestResolver,
-  Tag,
-  pipe,
-} from "bot/_common"
+import { Cache, Data, Duration, Effect, Layer, Tag } from "bot/_common"
 import { Discord, DiscordREST } from "dfx"
-import { DiscordRESTError, ResponseDecodeError } from "dfx/DiscordREST"
 
 export class GetMember extends Data.TaggedClass("GetMember")<{
   readonly guildId: Discord.Snowflake
@@ -20,31 +9,20 @@ export class GetMember extends Data.TaggedClass("GetMember")<{
 const make = Effect.gen(function* (_) {
   const rest = yield* _(DiscordREST)
 
-  interface GetMember
-    extends Request.Request<
-      DiscordRESTError | ResponseDecodeError,
-      Discord.GuildMember
-    > {
-    readonly _tag: "GetMember"
+  class GetMember extends Data.TaggedClass("GetMember")<{
     readonly guildId: Discord.Snowflake
     readonly userId: Discord.Snowflake
-  }
-  const GetMember = Request.tagged<GetMember>("GetMember")
+  }> {}
 
-  const resolver = RequestResolver.fromFunctionEffect(
-    ({ guildId, userId }: GetMember) =>
+  const cache = yield* _(
+    Cache.make(1000, Duration.days(1), ({ guildId, userId }: GetMember) =>
       Effect.flatMap(rest.getGuildMember(guildId, userId), _ => _.json),
+    ),
   )
-
-  const cache = yield* _(Request.makeCache(1000, Duration.days(1)))
 
   return {
     get: (guildId: Discord.Snowflake, userId: Discord.Snowflake) =>
-      pipe(
-        Effect.request(GetMember({ guildId, userId }), resolver),
-        Effect.withRequestCache(cache),
-        Effect.withRequestCaching("on"),
-      ),
+      cache.get(new GetMember({ guildId, userId })),
   } as const
 })
 
--- src/Summarizer.ts
+++ src/Summarizer.ts
@@ -1,6 +1,7 @@
 import { ChannelsCache, ChannelsCacheLive } from "bot/ChannelsCache"
 import { MemberCache, MemberCacheLive } from "bot/MemberCache"
 import {
+  Cause,
   Chunk,
   Data,
   Effect,
@@ -10,7 +11,7 @@ import {
   Stream,
   pipe,
 } from "bot/_common"
-import { Discord, DiscordREST, Ix, Log } from "dfx"
+import { Discord, DiscordREST, Ix } from "dfx"
 import { InteractionsRegistry, InteractionsRegistryLive } from "dfx/gateway"
 
 export class NotInThreadError extends Data.TaggedClass(
@@ -27,10 +28,10 @@ const make = Effect.gen(function* (_) {
   const channels = yield* _(ChannelsCache)
   const registry = yield* _(InteractionsRegistry)
   const members = yield* _(MemberCache)
+  const scope = yield* _(Effect.scope())
   const application = yield* _(
     Effect.flatMap(rest.getCurrentBotApplicationInformation(), _ => _.json),
   )
-  const scope = yield* _(Effect.scope())
 
   const getAllMessages = (channelId: string) =>
     pipe(
@@ -56,32 +57,24 @@ const make = Effect.gen(function* (_) {
       ),
 
       // only include normal messages
-      Stream.flatMap(_ => {
-        if (_.type === Discord.MessageType.THREAD_STARTER_MESSAGE) {
-          return Stream.succeed(_)
+      Stream.flatMapPar(Number.MAX_SAFE_INTEGER, msg => {
+        if (msg.type === Discord.MessageType.THREAD_STARTER_MESSAGE) {
+          return Effect.flatMap(
+            rest.getChannelMessage(
+              msg.message_reference!.channel_id!,
+              msg.message_reference!.message_id!,
+            ),
+            _ => _.json,
+          )
         } else if (
-          _.content !== "" &&
-          (_.type === Discord.MessageType.REPLY ||
-            _.type === Discord.MessageType.DEFAULT)
+          msg.content !== "" &&
+          (msg.type === Discord.MessageType.REPLY ||
+            msg.type === Discord.MessageType.DEFAULT)
         ) {
-          return Stream.succeed(_)
+          return Stream.succeed(msg)
         }
 
         return Stream.empty
-      }),
-
-      Stream.mapEffectPar(Number.MAX_SAFE_INTEGER, message => {
-        if (message.type !== Discord.MessageType.THREAD_STARTER_MESSAGE) {
-          return Effect.succeed(message)
-        }
-
-        return Effect.flatMap(
-          rest.getChannelMessage(
-            message.message_reference!.channel_id!,
-            message.message_reference!.message_id!,
-          ),
-          _ => _.json,
-        )
       }),
     )
 
@@ -90,31 +83,29 @@ const make = Effect.gen(function* (_) {
     thread: Discord.Channel,
     messages: Chunk.Chunk<Discord.Message>,
   ) =>
-    Effect.gen(function* (_) {
-      const messageContent = yield* _(
-        Effect.forEachParWithIndex(messages, (message, index) => {
-          const reply = pipe(
-            Option.fromNullable(message.message_reference),
-            Option.flatMap(ref =>
-              Chunk.findFirstIndex(messages, _ => _.id === ref.message_id),
-            ),
-            Option.map(
-              index => [Chunk.unsafeGet(messages, index), index + 1] as const,
-            ),
-          )
-          return summarizeMessage(thread, index + 1, message, reply)
-        }),
-      )
-
-      return `# ${thread.name}
+    Effect.map(
+      Effect.forEachParWithIndex(messages, (message, index) => {
+        const reply = pipe(
+          Option.fromNullable(message.message_reference),
+          Option.flatMap(ref =>
+            Chunk.findFirstIndex(messages, _ => _.id === ref.message_id),
+          ),
+          Option.map(
+            index => [Chunk.unsafeGet(messages, index), index + 1] as const,
+          ),
+        )
+        return summarizeMessage(thread, index + 1, message, reply)
+      }),
+      messageContent =>
+        `# ${thread.name}
 
 Thread started in: #${channel.name}<br />
 Thread started at: ${new Date(
-        thread.thread_metadata!.create_timestamp!,
-      ).toUTCString()}
+          thread.thread_metadata!.create_timestamp!,
+        ).toUTCString()}
 
-${messageContent.join("\n\n")}`
-    })
+${messageContent.join("\n\n")}`,
+    )
 
   const summarizeMessage = (
     thread: Discord.Channel,
@@ -126,13 +117,11 @@ ${messageContent.join("\n\n")}`
       const user = message.author
       const member = yield* _(members.get(thread.guild_id!, message.author.id))
       const username = member.nick ?? user.username
-      const content = `${index}: **${username}**, ${new Date(
-        message.timestamp,
-      ).toUTCString()}${Option.match(
+      const content = `${index}: **${username}**${Option.match(
         replyTo,
         () => "",
-        ([, index]) => ` (in reply to \\#${index})`,
-      )}<br />
+        ([, index]) => ` (replying to \\#${index})`,
+      )} &mdash; ${new Date(message.timestamp).toUTCString()}<br />
 ${message.content}`
 
       const mentions = yield* _(
@@ -185,10 +174,7 @@ ${message.content}`
         )
         formData.append(
           "payload_json",
-          JSON.stringify({
-            content: "Here is your summary!",
-            flags: Discord.MessageFlag.EPHEMERAL,
-          }),
+          JSON.stringify({ content: "Here is your summary!" }),
         )
 
         return rest.editOriginalInteractionResponse(
@@ -197,6 +183,16 @@ ${message.content}`
           { body: Http.body.formData(formData) },
         )
       }),
+      Effect.catchAllCause(cause =>
+        rest.editOriginalInteractionResponse(application.id, context.token, {
+          body: Http.body.json({
+            content:
+              "Could not create summary. Here are the full error details:\n\n```" +
+              Cause.pretty(cause) +
+              "\n```",
+          }),
+        }),
+      ),
     )
 
   const command = Ix.global(
