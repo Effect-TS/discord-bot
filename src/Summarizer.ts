c2e21907103893163acda9dde705aca379b8cbb3
Tim <hello@timsmart.co>
2023-05-23 14:07:44 +1200

    chore: improve ending code block regex (#22)

--- src/Summarizer.ts
+++ src/Summarizer.ts
@@ -138,7 +138,7 @@ ${message.content
   .replace(/```ts\b/g, "```typescript")
   .replace(/^```/, "\n```")
   .replace(/[^\n]```/gm, "\n\n```")
-  .replace(/([^\n])\n```/gm, "$1\n\n```")}`
+  .replace(/([^\n])\n```([^\n]*\n[^\n])/gm, "$1\n\n```$2")}`
 
       const mentions = yield* _(
         Effect.forEachPar(content.matchAll(/<@(\d+)>/g), ([, userId]) =>
