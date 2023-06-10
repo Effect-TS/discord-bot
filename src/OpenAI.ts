8f27c4983711376e1f060e952e68ce4ceab68f26
Tim Smart <tim.smart@arisechurch.com>
2023-06-10 17:21:47 +1200

    use OpenAI defaults for summary

--- src/OpenAI.ts
+++ src/OpenAI.ts
@@ -122,11 +122,6 @@ The title of this conversation is "${title}".`,
         _.createChatCompletion(
           {
             model: "gpt-3.5-turbo",
-            temperature: 1,
-            top_p: 1,
-            max_tokens: 256,
-            frequency_penalty: 0,
-            presence_penalty: 0,
             messages: [
               {
                 role: "system",
