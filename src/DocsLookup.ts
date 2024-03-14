5ec28dafc9e869f4ef8461ddb1528e88903ce1e3
Tim Smart <tim.smart@arisechurch.com>
2024-03-14 21:35:43 +1300

    revert to older dfx

--- src/DocsLookup.ts
+++ src/DocsLookup.ts
@@ -164,7 +164,7 @@ export const DocsLookupLive = Layer.effectDiscard(make).pipe(
 
 // schema
 
-class DocEntry extends Schema.Class<DocEntry>("DocEntry")({
+class DocEntry extends Schema.Class<DocEntry>()({
   doc: Schema.string,
   title: Schema.string,
   content: Schema.string,
