4e695b95c0beab37f3a1a4d3adb88c8291138a38
Tim Smart <hello@timsmart.co>
2025-11-14 21:41:44 +1300

    update effect

--- packages/shared/src/Sql.ts
+++ packages/shared/src/Sql.ts
@@ -1,8 +1,6 @@
 import { PgClient } from "@effect/sql-pg"
 import { Config } from "effect"
-import { constVoid } from "effect/Function"
 
 export const SqlClientLayer = PgClient.layerConfig({
-  url: Config.redacted("DATABASE_URL"),
-  onnotice: Config.succeed(constVoid)
+  url: Config.redacted("DATABASE_URL")
 })
