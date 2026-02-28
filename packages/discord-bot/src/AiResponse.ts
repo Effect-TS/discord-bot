b8089cbcf191822ea0ba98e3c78c495ad33445fc
Tim Smart <hello@timsmart.co>
2026-03-01 08:19:35 +1300

    try 5.1

--- packages/discord-bot/src/AiResponse.ts
+++ packages/discord-bot/src/AiResponse.ts
@@ -167,7 +167,7 @@ ${llmsMd}`
 
   const application = yield* DiscordApplication
   const rest = yield* DiscordREST
-  const chatModel = yield* OpenAiLanguageModel.model("gpt-5.2")
+  const chatModel = yield* OpenAiLanguageModel.model("gpt-5.1-chat-latest")
   const generate = Effect.fn("AiResponse.generate")(
     function*(
       context: Discord.APIInteraction,
